512c71796a78e37a86e0ab81e44d5b4a44fdb3ff
Kimheangchhun50 <kimheangchhun50@gmail.com>
2020-03-16 20:16:40 +0700

    Dashboard

--- index.php
+++ index.php
@@ -1,33 +1,176 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Sunsport Management System</title>
+<title>Sunsport Football Club</title>
 </head>
-<body>
-<center>
-	<h1>Sunsport Management System</h1>
-<form>
-		<div>
-			<label for="uname"> Username:</label>
-			<input type="text" placeholder="Enter your Username" name="uname">
 
-			<br><br>
-			
-			<label for="pwords"> Passwords:</label>
-			<input type="password" placeholder="Enter your Passwords" name="pwords">
+<style>
+body {
+  background-color: green;
+  color: white;
+  font-family: sans-serif;
+}
 
-			<br><br>
+h1 {
+font-size: 50px;
+}
+form.form1{
+	font-size: 18px;
+}
+input {
+	border: 3px solid darkgreen;
+}
 
-		    <label>
-		      <input type="checkbox" checked="checked" name="remember"> Remember me
-		    </label>
+ table{
+ 	width: 100%;
+ }
 
-    		<br><br>
+table, td, tr, th{
+ 	align-self: center;
+ 	border: 2px solid white;
+ }
+</style>
 
-			<button type="submit">Login</button>
+	<body>
+<div>
+<form id="form2">
 
-		</div>
-	</form>
-</center>
-</body>
+<table>
+	<tr>
+    <th><h4>Dashboard</h4></th>
+    <th><label name="date">Date:</label></th> 
+    <th colspan="4"><input style="width: 150px; height: 5 px;" type="date" name="date"></th>
+   
+    <th><p>Logout</p></th>   
+  	</tr>
+  <tr>
+  	<th>Reservation Board</th>
+    <th>Time Table</th>
+    <th>Field A</th> 
+    <th>Field B</th>
+    <th>Field C</th>
+    <th>Field D</th>
+    <th>Field E</th>    
+  </tr>
+
+  <tr>
+  	<th>Price Table</th>
+    <td>08:00-09:00 AM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+  	<th rowspan="13"></th>
+    <td>09:00-10:00 AM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>10:00-11:00 AM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>11:00-12:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>12:00-01:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>01:00-02:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+    <tr>
+    <td>02:00-03:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+    <tr>
+    <td>03:00-04:00 PM</td>
+    <th></th>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>04:00-05:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>05:00-06:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>06:00-07:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+    <tr>
+    <td>07:00-08:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+    <tr>
+    <td>08:00-09:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+    <tr>
+    <td>09:00-10:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+</table>
+</form>
+</div>
+	</body>
 </html>
+
---
+++ template/form-booking.php
@@ -0,0 +1,168 @@
+<style>
+body {
+  background-color: green;
+  color: white;
+  font-family: sans-serif;
+}
+
+h1 {
+font-size: 50px;
+}
+form.form1{
+  font-size: 18px;
+}
+input {
+  border: 3px solid darkgreen;
+}
+
+ table{
+  width: 100%;
+ }
+
+table, td, tr, th{
+  align-self: center;
+  border: 2px solid white;
+ }
+</style>
+
+<div>
+<form id="form2">
+
+<table>
+  <tr>
+    <th><h4>Dashboard</h4></th>
+    <th><label name="date">Date:</label></th> 
+    <th colspan="4"><input style="width: 150px; height: 5 px;" type="date" name="date"></th>
+   
+    <th><p>Logout</p></th>   
+    </tr>
+  <tr>
+    <th>Reservation Board</th>
+    <th>Time Table</th>
+    <th>Field A</th> 
+    <th>Field B</th>
+    <th>Field C</th>
+    <th>Field D</th>
+    <th>Field E</th>    
+  </tr>
+
+  <tr>
+    <th>Price Table</th>
+    <td>08:00-09:00 AM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <th rowspan="13"></th>
+    <td>09:00-10:00 AM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>10:00-11:00 AM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>11:00-12:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>12:00-01:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>01:00-02:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+    <tr>
+    <td>02:00-03:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+    <tr>
+    <td>03:00-04:00 PM</td>
+    <th></th>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>04:00-05:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>05:00-06:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>06:00-07:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+    <tr>
+    <td>07:00-08:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+    <tr>
+    <td>08:00-09:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+    <tr>
+    <td>09:00-10:00 PM</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+
+</table>
+</form>
+</div>
+
+
